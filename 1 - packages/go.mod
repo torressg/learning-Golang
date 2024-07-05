ab111183a9fad848f351a44a42c09e6ef2d6eb14
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-07-05 12:15:31 -0300

    learning about extern packages

--- 1 - packages/go.mod
+++ 1 - packages/go.mod
@@ -1,3 +1,5 @@
 module module
 
 go 1.22.5
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- 1 - packages/main.go
+++ 1 - packages/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo da Main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("teste@gmail.com")
+	fmt.Println((erro))
 }
